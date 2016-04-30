e731a714574fa55765a6d52f62b27fee09651279
lizsdregas <liz@sdregas.com>
2016-04-30 14:03:37 -0500

    testing users

--- scripts/example.js
+++ scripts/example.js
@@ -2,20 +2,27 @@
 
 module.exports = function(robot) {
 
-	// ask the bot if we have class today
+	// simple "hello" response
 	robot.hear(/hello\?/i, function(response) {
-		response.send('HELLO LIZ');
+		response.send('HEY THERE');
 	})
 
+  // simple "hello" response
+	robot.hear(/users\?/i, function(response) {
+    var users = users.list();
+		response.send(users); 
+	})
+
+  // if the bot hears "cat" it will reply with a random, cute cat picture
 	robot.hear(/cat/i, function(response) {
     function getRandomNumber(max, min) {
       return Math.floor(Math.random() * (max - min)) + min
     }
-    var randomWidth = getRandomNumber(1000, 500);
-    var randomHeight = getRandomNumber(500, 300);
+    var randomWidth = getRandomNumber(1200, 500);
+    var randomHeight = getRandomNumber(1000, 300);
     var imageUrl = 'http://placekitten.com/' + randomWidth + '/' + randomHeight;
 		response.reply(imageUrl);
-	}) 
+	})
 
 	// robot.respond() means the robot will only reply when at-mentioned or DMed
 	robot.respond(/who should i call on today\?/i, function(response) {
