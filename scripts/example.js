9428b9ccdd61e15ab2816c82cc6521300635b4e1
lizsdregas <liz@sdregas.com>
2016-04-30 13:52:56 -0500

    better random number calc

--- scripts/example.js
+++ scripts/example.js
@@ -7,12 +7,15 @@ module.exports = function(robot) {
 		response.send('HELLO LIZ');
 	})
 
-	robot.hear(/cats/i, function(response) {
-    var randomWidth = Math.random() * 200;
-    var randomHeight - Math.random() * 300;
-    var imageUrl = 'http://placekitten.com/' + randomWidth + '/' + randomHeight;    
+	robot.hear(/cat/i, function(response) {
+    function getRandomNumber(max, min) {
+      return Math.floor(Math.random() * (max - min)) + min
+    }
+    var randomWidth = getRandomNumber(1000, 500);
+    var randomHeight = getRandomNumber(500, 300);
+    var imageUrl = 'http://placekitten.com/' + randomWidth + '/' + randomHeight;
 		response.reply(imageUrl);
-	})
+	}) 
 
 	// robot.respond() means the robot will only reply when at-mentioned or DMed
 	robot.respond(/who should i call on today\?/i, function(response) {
