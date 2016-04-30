cd53dafa53725395c792f0c893550360459efd4f
lizsdregas <liz@sdregas.com>
2016-04-30 12:54:41 -0500

    First bot commit

---
+++ scripts/example.js
@@ -0,0 +1,20 @@
+module.exports = function(robot) {
+
+	// robot.hear() will have the robot listen in on any channel it's in
+	robot.hear(/hello/i, function(response) {
+		response.send("HELLO LIZ")
+	})
+
+	robot.hear(/i like (.+)/i, function(response) {
+		var like = response.match[1]
+		// response.reply() will have the robot at-mention you when it responds
+		response.reply('I also like ' + like)
+	})
+
+	// robot.respond() means the robot will only reply when at-mentioned or DMed
+	robot.respond(/who should i call on today\?/i, function(response) {
+		var user = robot.brain.userForName('gordo')
+		response.reply('Call on ' + user.real_name + '!')
+	})
+
+}
