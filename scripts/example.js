284d3a266735fecfa65fd1a05d8ca81b6e1202fc
lizsdregas <liz@sdregas.com>
2016-04-30 13:03:05 -0500

    updating bot

--- scripts/example.js
+++ scripts/example.js
@@ -8,7 +8,7 @@ module.exports = function(robot) {
 	robot.hear(/i like (.+)/i, function(response) {
 		var like = response.match[1]
 		// response.reply() will have the robot at-mention you when it responds
-		response.reply('I also like ' + like)
+		response.reply('I actually hate ' + like)
 	})
 
 	// robot.respond() means the robot will only reply when at-mentioned or DMed
